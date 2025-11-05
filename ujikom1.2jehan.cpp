dd0cdb32f2be12b3083ecb7b6c8ad473a5c7a622
UAD-Jehan <2500018004@webmail.uad.ac.id>
2025-11-05 09:47:00 +0700

    ujikom1.2jehan.cpp

--- ujikom1.2jehan.cpp
+++ ujikom1.2jehan.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(){
     // Deklarasi
-    int totaldetik, hari, jam, menit, detik;
-    // Input nilai totaldetik
-    cout << "Masukkan bilangan > 1 (satuan detik)    : "; cin >> totaldetik;
-        if(totaldetik>1){
-            detik = totaldetik;
+    long long int detikawal, hari, jam, menit, detik;
+    // Input nilai detikawal
+    cout << "Masukkan bilangan > 1 (satuan detik)    : "; cin >> detikawal;
+        if(detikawal>1){
+            detik = detikawal;
             // Inisialisasi var.hari
             hari = detik/86400;
             detik %= 86400;
@@ -19,7 +19,7 @@ int main(){
             detik %= 60; // sisa detik
             // Print output
             cout << "Hasil Konversi : " << endl;
-            cout << totaldetik << " detik    = " << hari << " Hari, " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
+            cout << detikawal << " detik    = " << hari << " Hari, " << jam << " Jam, " << menit << " Menit, " << detik << " Detik.";
         } else {
             cout << "Bilangan harus > 1 !" << endl;
         }
